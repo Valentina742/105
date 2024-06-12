dc862eae79779e59d53c41f8bc319a97a532a1b2
Valentina742 <153331379+Valentina742@users.noreply.github.com>
2024-06-11 20:48:53 -0500

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,10 @@
+//3. crear la función addUser
+
+function addUser() {
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    //firebase.database().ref("/").child(user_name).update({
+        //purpose: "agregando usuario"
+    //});
+    window.location = "kwitter_room.html";
+}
